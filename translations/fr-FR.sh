71305d2082c65f137ec8afb369004c69474e2c3d
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-29 01:51:58 +0200

    Delete fr-FR.sh

--- translations/fr-FR.sh
+++
@@ -1,4 +0,0 @@
-message_root='Erreur : ce script ne doit pas être exécuté avec les privilèges super-utilisateur.'
-message_mintupdate='Démarrage des mises à jour…'
-firefox_notification='Installer uBlock Origin et l’autoriser en mode navigation privée.'
-message_confirmation='Post-installation terminée. Merci de redémarrer l’ordinateur.'
