41f3783a0cb033d0fcc917bd5f88d72992295490
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-29 01:10:24 +0200

    Update config_fedora.sh

--- modules/distributions/config_fedora.sh
+++ modules/distributions/config_fedora.sh
@@ -1,17 +1,21 @@
 config_fedora() {
 ## Updates ##
-sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf autoremove -y
 flatpak update -y
 flatpak remove -y --unused
+# Add Flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 ## RPMFusion ##
 # Enable RPMFusion free and nonfree
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # Add the Appstream metadata
-sudo dnf groupupdate core
+sudo dnf groupupdate -y core
 # Install the complements multimedia packages needed by gstreamer enabled applications
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 # Install the sound-and-video complement packages needed by some applications
-sudo dnf groupupdate sound-and-video
+sudo dnf groupupdate -y sound-and-video
 # Install tainted repo
 sudo dnf install -y rpmfusion-free-release-tainted
 # Install DVD support
@@ -21,4 +25,9 @@ sudo dnf install -y rpmfusion-nonfree-release-tainted
 # Install missing firmwares
 sudo dnf install -y \*-firmware
 
+## Additional software ##
+sudo dnf install -y htop
+sudo dnf install -y firewall-config -y
+
+## Nvidia ##
 }
