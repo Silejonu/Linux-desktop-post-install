f401f66268b40788b0c289cfd8d28a9c04bd7755
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-29 01:58:23 +0200

    Create eng.sh

---
+++ translations/eng.sh
@@ -0,0 +1,4 @@
+  message_root='Error: do not run this script with superuser privileges.'
+  message_mintupdate='Starting updates…'
+  firefox_notification='Install uBlock Origin and allow it to run in private mode.'
+  message_confirmation='Post-install finished. Please reboot the computer.'
