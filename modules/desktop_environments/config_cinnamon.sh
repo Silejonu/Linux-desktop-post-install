1d21763be553e3fc68159675ac4c8ca539296134
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-26 17:12:05 +0200

    Update config_cinnamon.sh

--- modules/desktop_environments/config_cinnamon.sh
+++ modules/desktop_environments/config_cinnamon.sh
@@ -1,21 +1,15 @@
-## Explorateur de fichiers ##
-# Toujours afficher les vignettes d’aperçu des images/documents (y compris sur les périphériques externes)
-gsettings set org.nemo.preferences show-image-thumbnails always
-# Afficher les vignettes pour les documents faisant 64 Go au maximum (au lieu de 4 Go par défaut)
-gsettings set org.nemo.preferences thumbnail-limit 68719476736
-# Toujours afficher le nombre d'éléments (y compris sur les périphériques externes)
-gsettings set org.nemo.preferences show-directory-item-counts always
 ## Interface ##
-# Afficher la corbeille sur le bureau
+# Show Trash on the desktop
 gsettings set org.nemo.desktop trash-icon-visible true
-# Cacher le Poste de travail sur le bureau
+# Hide Computer on the desktop
 gsettings set org.nemo.desktop computer-icon-visible false
-# Afficher la date dans l'horloge
+# Show the date in the clock
 gsettings set org.cinnamon.desktop.interface clock-show-date true
-# Régler la mise en veille automatique sur 15 minutes
+# Set sleep delay to 15 minutes
 gsettings set org.cinnamon.desktop.session idle-delay 900
-## Sécurité & confidentialité ##
-# Supprimer automatiquement les fichiers temporaires après 30 jours
+
+## Security & privacy ##
+# Automatically delete temporary files (after 30 days by default)
 gsettings set org.cinnamon.desktop.privacy remove-old-temp-files true
-# Supprimer automatiquement les fichiers de la corbeille après 30 jours
+# Automatically delete old trash files (after 30 days by default)
 gsettings set org.cinnamon.desktop.privacy remove-old-trash-files true
