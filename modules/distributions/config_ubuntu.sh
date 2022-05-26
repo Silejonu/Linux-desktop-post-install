042a14bc5ca077ece6a6d97409614da503fc1590
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-26 16:03:12 +0200

    Update config_ubuntu.sh

--- modules/distributions/config_ubuntu.sh
+++ modules/distributions/config_ubuntu.sh
@@ -1,29 +1,28 @@
 ## Interface ##
-# Cacher le répertoire « snap » situé dans le dossier home
+# Hide the "snap" directory in /home
 grep -qx snap ~/.hidden &> /dev/null || echo snap >> ~/.hidden
 # Hide "firefox.tmp" in the Downloads directory
 grep -qx firefox.tmp $(xdg-user-dir DOWNLOAD)/.hidden &> /dev/null || echo firefox.tmp >> $(xdg-user-dir DOWNLOAD)/.hidden
-# Désactiver les messages d'erreur lorsqu'une application malfonctionne en arrière-plan
+# Disable error messages when an application crashes
 sudo sed -i 's/enabled=1/enabled=0/' /etc/default/apport
-## Gestionnaire de paquets ##
-# Mettre à jour la liste des paquets disponibles
+
+## Package management ##
+# Update the database
 sudo apt update
 # Prevent wslu from installing to avoid bug https://bugs.launchpad.net/ubuntu/+source/wslu/+bug/1971757
 sudo apt-mark hold wslu
-# Mettre à jour les paquets
+# Update the packages
 sudo apt upgrade -y
 # Install Firefox .deb to improve integration
 sudo apt install -y firefox
-# Installer les paquets propriétaires (codecs multimédias, pilotes wi-fi, etc.)
+# Install proprietary software and codecs
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install -y ubuntu-restricted-extras
-# Installer le support des DVD
+# Install DVD support
 sudo DEBIAN_FRONTEND=noninteractive apt install -y libdvd-pkg
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure libdvd-pkg
-# Installer une interface graphique au pare-feu
+# Install firewall GUI
 sudo apt install -y gufw
-# Install Firefox .deb package to circumvent this bug: https://www.reddit.com/r/Ubuntu/comments/uj3x52/psa_fix_filesdirectories_on_the_desktop_not/
-sudo apt install -y firefox
 # Change the favourites in Dash-to-Dock
 if [[ $(printf "%s" "${XDG_SESSION_DESKTOP}") == 'ubuntu' ]] ; then
   gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'libreoffice-startcenter.desktop', 'rhythmbox.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop']"
