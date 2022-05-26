94f3e8a55c5b62567db240219c2f63900f875138
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-26 16:37:03 +0200

    Update eng.sh

--- translations/eng.sh
+++ translations/eng.sh
@@ -1,5 +1,4 @@
 message_root='Error: do not run this script with superuser privileges.'
-message_mintupdate='Starting updates…'
 message_ublock_origin='Install uBlock Origin and allow it to run in private mode.'
 message_confirmation='Post-install finished. Please reboot the computer.'
 message_mint_mirrors='Select a local mirror.'
