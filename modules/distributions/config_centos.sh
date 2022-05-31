5c26717f9164aa7e2a8f4fad8645e857977735f3
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-31 20:03:13 +0200

    Create config_centos.sh

---
+++ modules/distributions/config_centos.sh
@@ -0,0 +1,7 @@
+# Enable EPEL
+sudo dnf install -y --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
+# Enable RPMFusion free and nonfree
+sudo dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
+
+# https://github.com/Silejonu/Linux-desktop-post-install/blob/main/modules/distributions/config_redhat_family.sh
+source ./modules/distributions/config_redhat_family.sh
