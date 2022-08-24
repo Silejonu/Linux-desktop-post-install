e2d786eaa1affaec4715bf8b84ad7620bde62aa1
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-08-25 00:47:04 +0200

    Update config_ubuntu_based.sh

--- modules/distributions/config_ubuntu_based.sh
+++ modules/distributions/config_ubuntu_based.sh
@@ -5,7 +5,7 @@ sudo ufw enable
 sudo apt install -y language-selector-common
 sudo apt install -y $(check-language-support)
 
-# Add HEIF support
+# Add HEIF images support
 sudo apt install -y heif-gdk-pixbuf heif-thumbnailer
 xdg-mime default $(xdg-mime query default image/jpg) image/heic
 
