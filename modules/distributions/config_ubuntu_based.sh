65d5ccc8661fef8d4f65da233ad2e342c4ed0325
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-08-25 00:08:39 +0200

    Update config_ubuntu_based.sh

--- modules/distributions/config_ubuntu_based.sh
+++ modules/distributions/config_ubuntu_based.sh
@@ -4,5 +4,9 @@ sudo ufw enable
 # Install localisation packages
 sudo apt install -y language-selector-common
 sudo apt install -y $(check-language-support)
+
+# Add HEIF support
+sudo apt install -y heif-gdk-pixbuf heif-thumbnailer
+
 # Remove obsolete packages
 sudo apt autoremove -y
