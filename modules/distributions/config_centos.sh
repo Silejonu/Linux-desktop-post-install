1914302986ba11d12af071dd9a70ef0c2dda51a6
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-08-28 02:06:49 +0200

    Update config_centos.sh

--- modules/distributions/config_centos.sh
+++ modules/distributions/config_centos.sh
@@ -12,5 +12,5 @@ source ./modules/distributions/config_redhat_family.sh
 ## Additional software ##
 # LibreOffice
 sudo dnf install -y libreoffice-{calc,draw,impress,writer}
-# FFmpeg for video codecs
-sudo dnf install -y ffmpeg
+# Video codecs in Firefox
+sudo dnf install -y ffmpeg gstreamer1-libav
