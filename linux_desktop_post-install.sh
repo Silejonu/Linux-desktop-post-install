db6a9c8aaac13ab8483578d0a780b9f8659feeee
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-11-19 11:54:04 +0100

    Update linux_desktop_post-install.sh

--- linux_desktop_post-install.sh
+++ linux_desktop_post-install.sh
@@ -1,72 +1,21 @@
 #!/usr/bin/bash
 
-version='v0.3.1'
-
-# Determine the language of the messages
 if [[ $(locale language) == French ]] ; then
-  source ./translations/fra.sh
+  echo
+  echo 'Ce script a changé d’adresse ! Il est désormais disponible ici :'
+  echo 'https://codeberg.org/Silejonu/Linux-desktop-post-install'
+  echo
+  echo 'Merci d’utiliser la commande suivante pour le lancer :'
+  echo
+  echo 'cd $(mktemp -d) && wget https://codeberg.org/Silejonu/Linux-desktop-post-install/archive/main.tar.gz && tar xf main.tar.gz && cd linux-desktop-post-install && bash ./linux_desktop_post-install.sh'
+  echo
 else
-  source ./translations/eng.sh
+  echo
+  echo 'This script has moved repos! It is now available at:'
+  echo 'https://codeberg.org/Silejonu/Linux-desktop-post-install'
+  echo
+  echo 'Please use the following command from now on:'
+  echo
+  echo 'cd $(mktemp -d) && wget https://codeberg.org/Silejonu/Linux-desktop-post-install/archive/main.tar.gz && tar xf main.tar.gz && cd linux-desktop-post-install && bash ./linux_desktop_post-install.sh'
+  echo
 fi
-
-# Handle notifications and terminal prompts
-message() {
-  notify-send --hint=int:transient:1 "Post-installation" "${1}"
-  echo "${1}"
-}
-
-# Prevent running the script as root
-if [[ ${UID} == 0 ]] ; then
-  message "${root}"
-  exit 1
-fi
-
-# Run the script for the appropriate desktop environment
-case $(printf "%s" "${XDG_SESSION_DESKTOP}") in
-  gnome|ubuntu)
-    source ./modules/desktop_environments/config_gnome.sh ;;
-  cinnamon)
-    source ./modules/desktop_environments/config_cinnamon.sh ;;
-  xfce|xubuntu)
-    source ./modules/desktop_environments/config_xfce.sh ;;
-esac
-
-# Run the script for the appropriate distribution
-case $(cat /etc/*-release 2> /dev/null | grep ^NAME | sed 's/NAME=//' | tr -d \"\') in
-  Ubuntu)
-    source ./modules/distributions/config_ubuntu.sh
-    source ./modules/distributions/config_ubuntu_based.sh
-    source ./modules/programs/config_timeshift.sh
-    # Manually install proprietary drivers
-    message "${install_proprietary_drivers}"
-    software-properties-gtk --open-tab=4
-    wait "$(pidof software-properties-gtk)"
-    ;;
-  'Linux Mint')
-    source ./modules/distributions/config_mint.sh
-    source ./modules/distributions/config_ubuntu_based.sh
-    source ./modules/programs/config_timeshift.sh
-    # Manually install proprietary drivers
-    message "${install_proprietary_drivers}"
-    nohup sudo mintdrivers &> /dev/null
-    until [[ $(pgrep mintdrivers | wc -l) == 0 ]] ; do
-      sleep 1
-    done
-    ;;
-  'Fedora Linux')
-    source ./modules/distributions/config_fedora.sh
-    ;;
-  'CentOS Stream')
-    source ./modules/distributions/config_centos.sh
-    ;;
-esac
-
-source ./modules/programs/config_firefox.sh
-
-echo
-message "${please_report_issues}"
-echo
-
-sleep 5
-
-message "${confirmation}"
